6574dca7eb6420c5bd97b49657a827eeba6650b3
gregory <gregory.pereiraa@gmail.com>
2022-11-22 21:58:08 +1100

    Names

---
+++ CalculationLibrary/Calculation.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculationLibrary;
+
+public class Calculation
+{
+     public double add(double x, double y)
+    {
+        return x + y;
+    }
+
+     public double substract(double x, double y)
+     {
+         return x - y;
+     }
+
+     public double divide(double x, double y)
+     {
+         return x / y;
+     }
+
+     public double multiply(double x, double y)
+     {
+         return x * y;
+     }
+}
