bb978ba6dd8b83bcbeb6e8768a596b4b6962c870
gregory <gregory.pereiraa@gmail.com>
2022-11-22 22:21:03 +1100

    Unit Test Implemented

---
+++ CalculationLibrary.Test/Calculation.Test.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+
+namespace CalculationLibrary.Test;
+
+public class Calculation_Test
+{
+    [Theory]
+    [InlineData(5,5,10)]
+    public void AddShouldReturnedExpectedValue(double x, double y, double expected)
+    {
+
+        Calculation calc = new Calculation();
+
+        double actual = calc.Add(x, y);
+
+        Assert.Equal(actual, expected);
+    }
+    [Theory]
+    [InlineData(5,5,0)]
+    public void SubstractShouldReturnedExpectedValue(double x, double y, double expected)
+    {
+
+        Calculation calc = new Calculation();
+
+        double actual = calc.Substract(x, y);
+
+        Assert.Equal(actual, expected);
+    }
+    
+    [Theory]
+    [InlineData(4,2,2)]
+    [InlineData(5,2,2.5)]
+    public void DivideShouldReturnedExpectedValue(double x, double y, double expected)
+    {
+
+        Calculation calc = new Calculation();
+
+        double actual = calc.Divide(x, y);
+
+        Assert.Equal(actual, expected);
+    }
+    
+    [Theory]
+    [InlineData(5,2,10)]
+    [InlineData(5,1.5,7.5)]
+    public void MultiplyShouldReturnedExpectedValue(double x, double y, double expected)
+    {
+
+        Calculation calc = new Calculation();
+
+        double actual = calc.Multiply(x, y);
+
+        Assert.Equal(actual, expected);
+    }
+
+}
--- CalculationLibrary/Calculation.cs
+++ CalculationLibrary/Calculation.cs
@@ -2,22 +2,22 @@
 
 public class Calculation
 {
-     public double add(double x, double y)
+     public double Add(double x, double y)
     {
         return x + y;
     }
 
-     public double substract(double x, double y)
+     public double Substract(double x, double y)
      {
          return x - y;
      }
 
-     public double divide(double x, double y)
+     public double Divide(double x, double y)
      {
          return x / y;
      }
 
-     public double multiply(double x, double y)
+     public double Multiply(double x, double y)
      {
          return x * y;
      }
